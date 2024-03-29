59b402a61b525e6f3f86372fc48f9908cc4d937f
0x1eef <0x1eef@protonmail.com>
2024-03-29 02:26:05 -0300

    Add API docs

--- lib/nanoc/weasyprint.rb
+++ lib/nanoc/weasyprint.rb
@@ -1,8 +1,15 @@
+##
+# nanoc-weasyprint.rb is a [nanoc](https://nanoc.app)
+# filter that integrates [weasyprint](https://github.com/Kozea/WeasyPrint)
+# into nanoc. The filter can create PDF documents
+# from markdown and other markup languages during the
+# [nanoc](https://nanoc.app) build process.
 module Nanoc::WeasyPrint
+  require_relative "weasyprint/filter"
+
   ##
   # @return (see Nanoc::WeasyPrint::Filter.default_argv)
   def self.default_argv
     Filter.default_argv
   end
-  require_relative "weasyprint/filter"
 end
